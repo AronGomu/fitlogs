7ca47f05fcb1a7feede4bdb6ce097875270c0a77
AronGomu <nflachaire@ctcpa.org>
2023-06-07 00:56:52 +0200

    wasting time on svg, centering items

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,32 @@
+<svg
+  class={$$props.class}
+  {focusable}
+  {width}
+  {height}
+  viewBox="0 0 {displayIcon['box']} {displayIcon['box']}">
+  {@html displayIcon['svg']}
+</svg>
+
+
+<script lang="ts">
+
+    export let name: string;
+    export let width: string = "1rem";
+    export let height: string = "1rem";
+    export let focusable: string = "false";
+
+
+    import DownChevron from './../assets/icons/down-chevron.svg';
+
+
+    let icons: {box: number, name: string, svg: string }[] = [
+        {box: 24, name: "down-chevron", svg: DownChevron}
+    ]
+
+    let displayIcon = icons.find((e) => e['name'] === name);
+
+    console.log(displayIcon);
+    
+
+</script>
+
--- src/routes/Workout.svelte
+++ src/routes/Workout.svelte
@@ -1,14 +1,22 @@
-{#each eas as ea}
-<div class="collapse bg-base-300 my-2">
-  <input type="checkbox" checked={ea.isOpen}/> 
-  <div class="collapse-title text-xl font-medium cursor-pointer">
-      {`${ea.name} - ${ea.sets.length} Sets - ${ea.getMaxWeight(weightMetric)}${getReducedStringMetric(weightMetric)} - ${ea.isOpen}`}
-  </div>
-  <div class="collapse-content">
-    <ExerciceForm exercice={ea.getExercice()}/>
-  </div>
+<div class="flex flex-col items-center">
+    {#each eas as ea}
+    <div class="collapse bg-base-300 my-2 w-5/6 override-collapse w-full">
+      <!-- <input type="checkbox" checked={ea.isOpen}/>  -->
+      <div class="collapse-title text-xl font-medium cursor-pointer text-primary w-full mx-2 override-collapse-title">
+        <div class="flex flex-row justify-between w-full">
+          <input type="text" value="{ea.name}" placeholder="Exercice Name" class="input input-bordered input-md w-2/3 max-w-xs text-primary" />
+          <img src={DownChevron} alt="" srcset="" height="20" width="20">
+        </div>
+        <span class="text-secondary text-sm">{`${ea.sets.length} Sets - ${ea.getMaxWeight(weightMetric)}${getReducedStringMetric(weightMetric)} - ${ea.isOpen}`}</span>
+      </div>
+      <div class="collapse-content">
+        <ExerciceForm exercice={ea.getExercice()}/>
+      </div>
+    </div>
+    {/each}
+
+  <button class="btn btn-primary w-4/5" on:click={newExercice}>New Exercice</button>
 </div>
-{/each}
 
 <ExerciceMenu />
 
@@ -22,6 +30,10 @@
   import { getReducedStringMetric } from "../shared/functions/Utilitary";
   import { WeightMetrics } from "../shared/enum/WeightMetrics";
   import { Weight } from "../shared/class/Weight";
+  // import InlineSVG from 'svelte-inline-svg'
+
+  import DownChevron from './../assets/icons/down-chevron.svg';
+  import Icon from "../lib/Icon.svelte";
 
   let weightMetric: WeightMetrics;
   $: {
@@ -44,8 +56,23 @@
     ]
   })
 
+  function newExercice(): void {
+    eas.push(new ExerciceAccordeon("", [], true));
+    eas = eas;
+  }
+
   function toggleExerciceSetAccordeon(ea: ExerciceAccordeon) {
     ea.isOpen = !ea.isOpen;
     eas = eas;
   }
 </script>
+
+<style>
+  .override-collapse {
+    width: 90%;
+  }
+
+  .override-collapse-title {
+    padding: 1rem;
+  }
+</style>
