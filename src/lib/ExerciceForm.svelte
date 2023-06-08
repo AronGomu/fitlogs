19913f4f4b0350fa95d8e0733918fbde68cc6e8b
AronGomu <nflachaire@ctcpa.org>
2023-06-09 00:22:28 +0200

    fusionning accordeon class, adding swiping technlogy with hammer,

--- src/lib/ExerciceForm.svelte
+++ src/lib/ExerciceForm.svelte
@@ -1,9 +1,9 @@
-{#if isMounted}
-{#each esas as set, index}
-<div class="collapse bg-base-300 p-1">
-  <input type="checkbox" checked={set.isOpen} class="override-collapse-title"/> 
+{#if isMounted && exercice && exercice.sets}
+{#each exercice.sets as set, i}
+<div class="collapse bg-base-300 p-1" bind:this={set.e}>
+  <input type="checkbox" checked={set.isOpen} class="override-collapse-title" on:mouseenter={() => {setHammer(set)}}/> 
   <div class="collapse-title font-medium bg-base-200 override-collapse-title cursor-pointer">
-      {`Set ${index+1}`}
+      {`Set ${i+1}`}
   </div>
   <div class="collapse-content bg-base-200 override-collapse-content z-10">
     <div class="flex flex-row items-center">
@@ -16,7 +16,7 @@
 {/each}
 
 <div class="flex justify-center w-full mt-2">
-  <button class="btn btn-primary w-4/5 override-addExerciceButton" on:click={addSet}>
+  <button class="btn btn-primary w-4/5 override-addExerciceButton" on:click={addSet} >
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" height=30 width=30 class="text-primary"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Edit / Add_Plus"> <path id="Vector" d="M6 12H12M12 12H18M12 12V18M12 12V6" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="--darkreader-inline-stroke: #e8e6e3; color: white" data-darkreader-inline-stroke=""></path> </g> </g></svg>
     Add Set
   </button>
@@ -25,41 +25,56 @@
 
 <script lang="ts">
   import { onMount } from "svelte";
-  import { Exercice } from "../shared/class/Exercice";
-  import { ExerciceSetAccordeon } from "../shared/class/ExerciceSetAccordeon";
   import InputNumber from "./InputNumber.svelte";
   import { wm } from './../shared/store/settingsStore';
   import type { WeightMetrics } from "../shared/enum/WeightMetrics";
   import { Weight } from "../shared/class/Weight";
 
+  import { Exercice } from "../shared/class/Exercice";
+  import { Set } from "../shared/class/Set";
+
+  import Hammer from 'hammerjs';
+
+  // import store
   let weightMetric: WeightMetrics;
-  $: {
-    weightMetric = $wm;
-  }
+  $: { weightMetric = $wm; }
+
+  // defining values
 
   let isMounted: boolean = false;
 
   /** Exercice that must be passed in argument. */
   export let exercice: Exercice;
-  /** List Exercice Set Accordeon extracted from the exercice given in input. */
-  let esas: ExerciceSetAccordeon[];
 
   onMount(() => {
-    if (exercice.sets) {
-      esas = [];
-      for (const es of exercice.sets) {
-        esas.push(new ExerciceSetAccordeon(es.repetitions, es.weight, true))
-      }
-    }
-
     isMounted = true;
   })
 
   function addSet(): void {
     let lastWeight: Weight = new Weight(0, weightMetric);
-    if (esas && esas.length > 0) lastWeight.weight = esas[esas.length-1].weight.weight;
-    esas.push(new ExerciceSetAccordeon(0, lastWeight, true));
-    esas = esas;
+    if (exercice.sets && exercice.sets.length > 0) lastWeight.weight = exercice.sets[exercice.sets.length-1].weight.weight;
+    exercice.sets.push(new Set(0, lastWeight, true));
+    exercice.sets = exercice.sets;
+  }
+
+  function setHammer(set: Set): void {
+    console.log("setHammer");
+    console.log(set);
+    
+    let h = new Hammer(set.e)
+    h.on('swipeleft', () => deleteSet(set))
+    h.on('swiperight', () => deleteSet(set))
+  }
+
+  function deleteSet(set: Set): void {
+    console.log("deleteSet");
+    for (let i = 0; i < exercice.sets.length; i++) {
+      if (exercice.sets[i] === set) {
+        exercice.sets.splice(i,1);
+        exercice.sets = exercice.sets;
+        return;
+      }
+    }
   }
 
 </script>
