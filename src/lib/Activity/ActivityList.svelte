73a6ded3bc09ebe43e10a019385fe1f3ee661ace
Nathan FLACHAIRE <nathan.flachaire@sogelink.com>
2024-02-06 09:37:37 +0100

    linkin ActivityDate imports

---
+++ src/lib/Activity/ActivityList.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import type { Activity } from "../../shared/class/Activity/Activity";
+	import { getActivitiesFromDatabase } from "../../shared/functions/Database";
+	import ActivityForm from "./ActivityForm.svelte";
+	import { ActivityDate } from "../../shared/class/Activity/ActivityDate";
+
+	import { createEventDispatcher } from "svelte";
+	const dispatch = createEventDispatcher();
+
+	export let activities: Activity[] = null;
+
+	let selectedDate: ActivityDate = null;
+
+	// UI Stuff
+	let activityFormDialog: HTMLElement;
+	function showActivityFormDialog(
+		year: number,
+		month: number,
+		day: number,
+		asModal = true,
+	) {
+		try {
+			selectedDate = new ActivityDate(year, month, day);
+			activityFormDialog[asModal ? "showModal" : "show"]();
+		} catch (e) {
+			throw new Error(e);
+		}
+	}
+
+	function refreshActivities() {
+		dispatch("refreshActivities");
+	}
+</script>
+
+<button
+	class="btn btn-primary"
+	on:click={() => showActivityFormDialog(null, null, null)}
+	>Log Activity</button
+>
+
+{#if activities}
+	<div class="h-full flex flex-col overflow-hidden">
+		<div class="flex-1 overflow-y-auto">
+			<table class="table">
+				<!-- head -->
+				<thead>
+					<tr>
+						<th>
+							<span class="font-bold"
+								>Date</span
+							>
+						</th>
+						<th>
+							<span class="font-bold"
+								>Weight</span
+							>
+						</th>
+						<th>
+							<span class="font-bold"
+								>Calories</span
+							>
+						</th>
+						<th>
+							<span class="font-bold"
+								>Steps</span
+							>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each activities as a}
+						<tr
+							on:click={() =>
+								showActivityFormDialog(
+									a.year,
+									a.month,
+									a.day,
+								)}
+						>
+							<td
+								>{a.year}-{a.month}-{a.day}</td
+							>
+							<td>{a.weight}</td>
+							<td>{a.calories}</td>
+							<td>{a.steps}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<!-- content here -->
+{/if}
+
+<dialog id="modal" class="modal" bind:this={activityFormDialog}>
+	<form method="dialog" class="modal-box">
+		<ActivityForm
+			activityDate={selectedDate}
+			on:saveActivity={() => refreshActivities()}
+		/>
+	</form>
+	<form method="dialog" class="modal-backdrop">
+		<button
+			on:click={() => {
+				refreshActivities();
+			}}>close</button
+		>
+	</form>
+</dialog>
---
+++ src/shared/class/Activity/ActivityDate.ts
@@ -0,0 +1,3 @@
+export class ActivityDate {
+    public constructor(public year: number, public month: number, public day: number) { }
+}
