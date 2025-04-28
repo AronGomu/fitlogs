6d32457e6b5e8b3d7d61249ece024c9d32eee5e0
nathan flachaire <nathan.flachaire.prestataire@it-ce.fr>
2025-04-28 09:26:52 +0200

    removing test because its for ui only

--- src/routes/ActivityPage.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test } from 'vitest'
-import { render } from '@testing-library/svelte';
-import ActivityPage from './ActivityPage.svelte';
-
-test('it renders activity page', () => {
-  const activityPage = render(ActivityPage);
-  expect(1).toBe(1);
-  // console.log(activityPage);
-});
